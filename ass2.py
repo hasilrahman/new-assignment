1f7ec0734a4dda5c5c368ee8b1cb97db77b3ab03
hasilrahman <79070659+hasilrahman@users.noreply.github.com>
2021-02-16 20:48:25 +0530

    Update ass2.py

--- ass2.py
+++ ass2.py
@@ -1,12 +1,21 @@
-class Vechile:
-	def __init__(self,name,petrol):
-		self.name = name
-	def fare(self):
-	return self.name
-	def petrol(self):
-	return self.petrol
-class Bus(Vechile):
-	def isBus(self):
-	
-sbus = Bus("school benz",100)
-print("busname",sbus)
+
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+    def fare(self):
+        return self.name
+    def run(self):
+    	return self.mileage 
+    def car(self):
+    	return self.capacity	   
+  
+class Bus(Vehicle):
+    
+
+School_bus = Bus("School Volvo", 12, 50)
+print("bus name", School_bus.fare())
+print("total mileage is:",School_bus.run())
+print("total capacity:",School_bus.car())
