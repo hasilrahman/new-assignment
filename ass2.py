efe9835ba6134e935c8e4fea145301f495db64b0
hasilrahman <79070659+hasilrahman@users.noreply.github.com>
2021-02-16 11:33:27 +0530

    Add files via upload

---
+++ ass2.py
@@ -0,0 +1,12 @@
+class Vechile:
+	def __init__(self,name,petrol):
+		self.name = name
+	def fare(self):
+	return self.name
+	def petrol(self):
+	return self.petrol
+class Bus(Vechile):
+	def isBus(self):
+	
+sbus = Bus("school benz",100)
+print("busname",sbus)
