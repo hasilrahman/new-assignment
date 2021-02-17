90c5b2efeb23c1cf9d34569c24ad85fa799d4cae
hasilrahman <79070659+hasilrahman@users.noreply.github.com>
2021-02-17 11:30:38 +0530

    Update ass18.py

--- ass18.py
+++ ass18.py
@@ -1,6 +1,17 @@
-num = int(input("Enter a number: "))
-mod = num % 2
-if mod > 0:
-    print("This is an odd number.")
-else:
-    print("This is an even number.")
+ef splitevenodd(A): 
+   evenlist = [] 
+   oddlist = [] 
+   for i in A: 
+      if (i % 2 == 0): 
+         evenlist.append(i) 
+      else: 
+         oddlist.append(i) 
+   print("Even lists:", evenlist) 
+   print("Odd lists:", oddlist) 
+A=list()
+n=int(input("Enter the size of the First List ::"))
+print("Enter the Element of First  List ::")
+for i in range(int(n)):
+   k=int(input(""))
+   A.append(k)
+splitevenodd(A)
